4299d3268e55f9cbce645681d73e49abd34a60fd
Niranjana <niranjanasrajeev@gmail.com>
2023-03-27 17:34:10 +0200

    added funciton declarations

--- functions.h
+++ functions.h
@@ -15,5 +15,10 @@ typedef struct {
     player players[5];
 } checkerboard;
 
+void initialize_checkerboard(checkerboard* board, int num_players, int board_size);  //Initialize the board in the beginning of the game
+void display_checkerboard(checkerboard board); //To display the board 
+void log_action(char* message)//To log messages
+void move_player(player *currentplayer, checkerboard *board, int x, int y) //To move player to a location
+void check_adjacent_players(player *player1, player *player2, checkerboard *board);//check if adjacent and play rock-paper-scissors
 
 
