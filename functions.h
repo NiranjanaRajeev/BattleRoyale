6a1da407e4d7dbe0d8f98250f4f22ae9f1115d41
Niranjana <niranjanasrajeev@gmail.com>
2023-03-28 09:55:33 +0200

    modified functions

--- functions.h
+++ functions.h
@@ -4,21 +4,20 @@
 //the struct for the player
 typedef struct {                
     char name[20];
-    int points;
+    int life_points;
     int x;
     int y;
 } player; 
 
-//the struct for the board
-typedef struct {                
-    int size;
-    player players[5];
-} checkerboard;
-
-void initialize_checkerboard(checkerboard* board, int num_players, int board_size);  //Initialize the board in the beginning of the game
-void display_checkerboard(checkerboard board); //To display the board 
-void log_action(char* message)//To log messages
-void move_player(player *currentplayer, checkerboard *board, int x, int y) //To move player to a location
-void check_adjacent_players(player *player1, player *player2, checkerboard *board);//check if adjacent and play rock-paper-scissors
-
+void initialize(int num_players, int board_length, int board_width, player **players_array);  //Initialize the board in the beginning of the game
+void log_action(char* message);//To log messages
+void move_player(player *currentplayer,player **players_array); //To move player to a location
+int is_tile_out_of_bounds(int x, int y, int board_length, int board_width);
+int is_tile_available(int x, int y, player **players_array); //To know if the tile is or out of bounds, and if it's not, to know if the tile is occupied by another player
+void check_adjacent_tiles(player *currentplayer, player **players_array);//check adjacent tiles
+void check_tile(int x, int y,player **players_array);//Check if tile is occupied and fight if there is a player
+void fight(player *player1, player *player2); // fight and the first who wins two times wins the fight, the other loses hp
+void display_board( player **players_array); //To display the board 
+void add_player(int index, player **players_array); //To add a player to the array
+void delete_player(int index, player **players_array); //To delete a player to the array
 
