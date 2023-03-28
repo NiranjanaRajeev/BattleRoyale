59baff2b588fe123d5880713804af9c9bcbc8d6b
Niranjana <niranjanasrajeev@gmail.com>
2023-03-28 17:16:25 +0200

    modified arguments of move_player

--- include/functions.h
+++ include/functions.h
@@ -13,7 +13,7 @@ int is_tile_out_of_bounds(int x, int y, int board_length, int board_width);//To 
 int is_tile_available(int x, int y, player *players_array,int board_length, int board_width,int num_players); //to know if the tile is occupied by another player
 void initialize(int num_players, int board_length, int board_width, player *players_array);  //Initialize the board in the beginning of the game
 void log_action(char* message);//To log messages
-void move_player(player *currentplayer,player **players_array); //To move player to a location
+void move_player(int index,player *players_array); //To move player to a location
 void check_adjacent_tiles(player *currentplayer, player **players_array);//check adjacent tiles
 void check_tile(int x, int y,player **players_array);//Check if tile is occupied and fight if there is a player
 void fight(player *player1, player *player2); // fight and the first who wins two times wins the fight, the other loses hp
