ca8eaa0e49ac551cb06c4666d844112e7d0bd42d
Niranjana <niranjanasrajeev@gmail.com>
2023-03-28 16:32:32 +0200

    without add_player function

--- include/functions.h
+++ include/functions.h
@@ -10,8 +10,7 @@ typedef struct {
 } player; 
 
 int is_tile_out_of_bounds(int x, int y, int board_length, int board_width);//To know if the tile is out of bounds
-int is_tile_available(int x, int y, player **players_array,int board_length, int board_width,int num_players); //to know if the tile is occupied by another player
-void add_player(int index, player *players_array,player *new_player); //To add a player to the array
+int is_tile_available(int x, int y, player *players_array,int board_length, int board_width,int num_players); //to know if the tile is occupied by another player
 void initialize(int num_players, int board_length, int board_width, player *players_array);  //Initialize the board in the beginning of the game
 void log_action(char* message);//To log messages
 void move_player(player *currentplayer,player **players_array); //To move player to a location
@@ -20,4 +19,3 @@ void check_tile(int x, int y,player **players_array);//Check if tile is occupied
 void fight(player *player1, player *player2); // fight and the first who wins two times wins the fight, the other loses hp
 void display_board( player **players_array); //To display the board 
 void delete_player(int index, player **players_array); //To delete a player from the array
-
