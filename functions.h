c95fb4cfb26cc6ff62a4a8b704e84d9cae0df505
Niranjana <niranjanasrajeev@gmail.com>
2023-03-28 10:19:10 +0200

    added comment for function is_tile_out_of_bounds

--- functions.h
+++ functions.h
@@ -9,15 +9,15 @@ typedef struct {
     int y;
 } player; 
 
+int is_tile_out_of_bounds(int x, int y, int board_length, int board_width);//To know if the tile is out of bounds
+int is_tile_available(int x, int y, player **players_array); //to know if the tile is occupied by another player
 void initialize(int num_players, int board_length, int board_width, player **players_array);  //Initialize the board in the beginning of the game
 void log_action(char* message);//To log messages
 void move_player(player *currentplayer,player **players_array); //To move player to a location
-int is_tile_out_of_bounds(int x, int y, int board_length, int board_width);
-int is_tile_available(int x, int y, player **players_array); //To know if the tile is or out of bounds, and if it's not, to know if the tile is occupied by another player
 void check_adjacent_tiles(player *currentplayer, player **players_array);//check adjacent tiles
 void check_tile(int x, int y,player **players_array);//Check if tile is occupied and fight if there is a player
 void fight(player *player1, player *player2); // fight and the first who wins two times wins the fight, the other loses hp
 void display_board( player **players_array); //To display the board 
 void add_player(int index, player **players_array); //To add a player to the array
-void delete_player(int index, player **players_array); //To delete a player to the array
+void delete_player(int index, player **players_array); //To delete a player from the array
 
